9c86cbeddb9da86c3d7440255937919836796c32
GeraltOfRivia3991 <106952994+San2022-117@users.noreply.github.com>
2022-06-14 09:24:51 +0200

    Added major data
    city,temperature, humidity, windspeed, pressure

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 alert("Looks like it works!");
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let pressureElement = document.querySelector("#pressure");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  pressureElement.innerHTML = response.data.main.pressure;
+  windElement.innerHTML = response.data.wind.speed;
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+
+let apiKey = "a46a064ebf3d176c277f7b42ebc41329";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Perth&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
